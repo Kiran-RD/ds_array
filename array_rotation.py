d2f58b18436be1e940b9d72c54e7e21bb8ca4893
Kiran-RD <kiran.r.diggavi@gmail.com>
2020-11-02 00:01:24 +0530

    Updated

--- array_rotation.py
+++ array_rotation.py
@@ -1,48 +1,43 @@
 # Python3 program to rotate an array by
 # d elements
 # Function to left rotate arr[] of size n by d
-def leftRotate(arr, d, n):
+
+def left_rotate_approach_1(arr, d):
+    # Removing d%n number of elements from left side of the list
+    # and placing then on the right of the list
+    n = len(arr)
     d = d % n
-    g_c_d = gcd(d, n)
-    for i in range(g_c_d):
-
-        # move i-th values of blocks
-        temp = arr[i]
-        j = i
-        while 1:
-            k = j + d
-            if k >= n:
-                k = k - n
-            if k == i:
-                break
-            arr[j] = arr[k]
-            j = k
-        arr[j] = temp
-
-    # UTILITY FUNCTIONS
+    return arr[d:] + arr[:d]
 
 
-# function to print an array
-def printArray(arr, size):
-    for i in range(size):
-        print("% d" % arr[i], end=" ")
-
-    # Fuction to get gcd of a and b
-
-
-def gcd(a, b):
-    if b == 0:
-        return a;
-    else:
-        return gcd(b, a % b)
-
-    # Driver program to test above functions
+def left_rotate_approach_2(arr, d):
+    # Running a for loop d%n number of times and in every iteration
+    # the left most element is moved to rightmost position and
+    # every element is shifted to left by 1
+    n = len(arr)
+    d = d % n
+    for _ in range(d):
+        temp = arr[0]
+        for i in range(1, n):
+            arr[i-1] = arr[i]
+        arr[-1] = temp
+    return arr
 
 
-arr = [1, 2, 3, 4, 5, 6, 7]
-n = len(arr)
-d = 2
-leftRotate(arr, d, n)
-printArray(arr, n)
+def main():
+    arr = list(map(int, input().split()))
+    d = int(input())
+    print(left_rotate_approach_1(arr, d))
+    print(left_rotate_approach_2(arr, d))
 
-# This code is contributed by Shreyanshi Arun
+
+if __name__ == '__main__':
+    main()
+
+# Test data
+# Input ------------------------
+# 1 2 3 4 5 6 7
+# 17
+# Output ------------------------
+# [4, 5, 6, 7, 1, 2, 3]
+# [4, 5, 6, 7, 1, 2, 3]
